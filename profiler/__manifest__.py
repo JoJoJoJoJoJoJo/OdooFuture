3f7a7f7c6b884e727c2601858a46d8c6e3bd2034
jonathan.wang <jonathan_wang@ehsy.com>
2021-12-24 16:07:11 +0800

    feat: initial commit

---
+++ profiler/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Future Feature - Profiler',
+    'version': '10.0.0.0.0',
+    'license': 'LGPL-3',
+    'summary': """ Bump future odoo features to low version: Profiler """,
+    'author': 'Joseph Wang',
+    'support': 'whr0510@qq.com',
+    'depends': [
+        'base',
+    ],
+    'installable': False,
+    'Application': False,
+}
